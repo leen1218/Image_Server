394608f1a4214da3e5733b1e76fbefe12d218c65
enli <enli@microstrategy.com>
2016-12-16 20:54:59 +0800

    upload image to qiniu

--- server/models/image_mgr.py
+++ server/models/image_mgr.py
@@ -6,8 +6,8 @@
 # Created on  Dec 16, 2016
 #
 
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
 from utils.logger_helper import logger
-from qiniu import Auth
 import urllib
 
 """Global Image Manager Instance"""
@@ -39,10 +39,32 @@ class ImageManager(object):
 		pass
 
 	def addImage(self, image):
+		self.downloadImageFromWeChatServer(image)  # For Test
+		self.uploadImageToQiniu(image)		# For Test
+		# self._image_queue.append(image)
+
+	def downloadImageFromWeChatServer(self, image):
 		url = self._weixin_image_download_url % (image["token"], image["id"])
 		local_path = image["local_path"]
 		urllib.urlretrieve(url, local_path)
-		# self._image_queue.append(image)
+		return local_path
 
-	def downloadAndUploadImage(self):
-		pass
+	def	uploadImageToQiniu(self, image):
+		# 构建鉴权对象
+		q = Auth(self._access_key, self._secret_key)
+
+		# 上传到七牛后保存的文件名
+		key = '%s.jpg' % image["id"]
+
+		# 生成上传 Token，可以指定过期时间等
+		token = q.upload_token(self._bucket_domain, key, 3600)
+
+		# 要上传文件的本地路径
+		localfile = image["local_path"]
+
+		ret, info = put_file(token, key, localfile)
+		logger.debug("ImageManager : upload image to qiniu" + info)
+		assert ret['key'] == key
+		assert ret['hash'] == etag(localfile)
+
+
